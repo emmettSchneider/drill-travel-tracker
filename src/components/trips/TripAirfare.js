b9f28499b35c1cd79db066e143841c20cde07410
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-02-13 13:42:18 -0600

    airfare form made, button handler is next

--- src/components/trips/TripAirfare.js
+++ src/components/trips/TripAirfare.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Grid, Form, Header, Segment, Image, Button, Icon } from 'semantic-ui-react'
+import airplane from "./airplane-placeholder.png"
 
 export default class TripAirfare extends Component {
 
@@ -32,34 +33,47 @@ export default class TripAirfare extends Component {
   }
 
   render() {
-    console.log(this.props)
+    console.log('tripId =' + this.props.match.params.tripId)
     return (
-      <React.Fragment>
-        <Form>
-          <Form.Field>
-            <label>Airfare</label>
-            <input placeholder='Airfare (total)'
-              id='airfare'
-              onChange={this.handleFieldChange}
-              type='number'
-              icon='plane'
-              iconPosition='left'
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Distance from home to destination</label>
-            <input placeholder='One-way distance (miles)'
-              id='tripMiles'
-              onChange={this.handleFieldChange}
-              type='number'
-            />
-          </Form.Field>
-          <Button type='submit'
-            onClick={() => this.patchAirfare()}
-          >Add airfare</Button>
-        </Form>
+      <div className='airfare-form'>
+        {/*
+    Heads up! The styles below are necessary for the correct render of this example.
+    You can do same with CSS, the main idea is that all the elements up to the `Grid`
+    below must have a height of 100%.
+  */}
+        <style>{`
+    body > div,
+    body > div > div,
+    body > div > div > div.login-form {
+      height: 100%;
+    }
+  `}</style>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='left'>
+              <Image src={airplane} /> Add airfare to this trip
+      </Header>
+            <Form size='large' >
+              <Segment stacked>
+                <Form.Input
+                  placeholder='Airfare (total)'
+                  id='airfare'
+                  onChange={this.handleFieldChange}
+                  type='number'
+                  icon='plane'
+                  iconPosition='left'
+                />
+                <Button type='submit'
+                  color='teal'
+                  fluid size='large'
+                  onClick={() => this.patchAirfare()}
+                >Add airfare</Button>
+              </Segment>
+            </Form>
 
-      </React.Fragment >
+          </Grid.Column>
+        </Grid>
+      </div>
     )
   }
 }
